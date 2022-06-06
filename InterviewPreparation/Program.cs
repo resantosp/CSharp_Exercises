25f7a8cdb6fa326dc31f0a4e199a875e2d96f563
Renata Santos <renatados.santosp@gmail.com>
2022-06-06 19:04:41 -0300

    Day1

---
+++ InterviewPreparation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+static void plusMinus(List<int> arr)
+{
+    double nTotal = 0;
+    double positive = 0;
+    double negative = 0;
+    double zero = 0;
+
+    foreach (int number in arr)
+    {
+        if (number > 0)
+        {
+            positive++;
+        }
+        else if (number == 0)
+        {
+            zero++;
+        }
+        else
+        {
+            negative++;
+        }
+
+        nTotal++;
+    }
+
+    Console.WriteLine($"{(positive / nTotal).ToString("F6")}");
+    Console.WriteLine($"{(negative / nTotal).ToString("F6")}");
+    Console.WriteLine($"{(zero / nTotal).ToString("F6")}");
+
+}
+
+static void Main()
+{
+
+}
